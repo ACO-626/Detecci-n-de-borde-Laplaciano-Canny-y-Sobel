a29282ac2be354de2f1cee55d570e0934a36ea48
ACO <54153297+ACO-626@users.noreply.github.com>
2020-11-22 01:36:24 -0600

    Add files via upload

---
+++ FormPrincipal.Designer.cs
@@ -0,0 +1,176 @@
+﻿namespace Laplacian_Canny_Sobel_edge_detection
+{
+    partial class FormPrincipal
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.archivoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.importarToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.salidrToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.bordeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.cannyToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.sobelToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.laplacianToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.imageBox1 = new Emgu.CV.UI.ImageBox();
+            this.valoresCannyToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.archivoToolStripMenuItem,
+            this.bordeToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(956, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // archivoToolStripMenuItem
+            // 
+            this.archivoToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.importarToolStripMenuItem,
+            this.salidrToolStripMenuItem});
+            this.archivoToolStripMenuItem.Name = "archivoToolStripMenuItem";
+            this.archivoToolStripMenuItem.Size = new System.Drawing.Size(60, 20);
+            this.archivoToolStripMenuItem.Text = "Archivo";
+            // 
+            // importarToolStripMenuItem
+            // 
+            this.importarToolStripMenuItem.Name = "importarToolStripMenuItem";
+            this.importarToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.importarToolStripMenuItem.Text = "Importar";
+            this.importarToolStripMenuItem.Click += new System.EventHandler(this.importarToolStripMenuItem_Click);
+            // 
+            // salidrToolStripMenuItem
+            // 
+            this.salidrToolStripMenuItem.Name = "salidrToolStripMenuItem";
+            this.salidrToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.salidrToolStripMenuItem.Text = "Salir";
+            this.salidrToolStripMenuItem.Click += new System.EventHandler(this.salidrToolStripMenuItem_Click);
+            // 
+            // bordeToolStripMenuItem
+            // 
+            this.bordeToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.cannyToolStripMenuItem,
+            this.sobelToolStripMenuItem,
+            this.laplacianToolStripMenuItem,
+            this.valoresCannyToolStripMenuItem});
+            this.bordeToolStripMenuItem.Name = "bordeToolStripMenuItem";
+            this.bordeToolStripMenuItem.Size = new System.Drawing.Size(50, 20);
+            this.bordeToolStripMenuItem.Text = "Borde";
+            // 
+            // cannyToolStripMenuItem
+            // 
+            this.cannyToolStripMenuItem.Name = "cannyToolStripMenuItem";
+            this.cannyToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.cannyToolStripMenuItem.Text = "Canny";
+            this.cannyToolStripMenuItem.Click += new System.EventHandler(this.cannyToolStripMenuItem_Click);
+            // 
+            // sobelToolStripMenuItem
+            // 
+            this.sobelToolStripMenuItem.Name = "sobelToolStripMenuItem";
+            this.sobelToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.sobelToolStripMenuItem.Text = "Sobel";
+            this.sobelToolStripMenuItem.Click += new System.EventHandler(this.sobelToolStripMenuItem_Click);
+            // 
+            // laplacianToolStripMenuItem
+            // 
+            this.laplacianToolStripMenuItem.Name = "laplacianToolStripMenuItem";
+            this.laplacianToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.laplacianToolStripMenuItem.Text = "Laplacian";
+            this.laplacianToolStripMenuItem.Click += new System.EventHandler(this.laplacianToolStripMenuItem_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.imageBox1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(0, 24);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(956, 572);
+            this.panel1.TabIndex = 1;
+            // 
+            // imageBox1
+            // 
+            this.imageBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.imageBox1.Location = new System.Drawing.Point(0, 0);
+            this.imageBox1.Name = "imageBox1";
+            this.imageBox1.Size = new System.Drawing.Size(956, 572);
+            this.imageBox1.TabIndex = 2;
+            this.imageBox1.TabStop = false;
+            // 
+            // valoresCannyToolStripMenuItem
+            // 
+            this.valoresCannyToolStripMenuItem.Name = "valoresCannyToolStripMenuItem";
+            this.valoresCannyToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.valoresCannyToolStripMenuItem.Text = "Valores Canny";
+            this.valoresCannyToolStripMenuItem.Click += new System.EventHandler(this.valoresCannyToolStripMenuItem_Click);
+            // 
+            // FormPrincipal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(15)))), ((int)(((byte)(15)))), ((int)(((byte)(18)))));
+            this.ClientSize = new System.Drawing.Size(956, 596);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "FormPrincipal";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Canny-Sobel-Laplacian-Edge-detection";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem archivoToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem importarToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem salidrToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem bordeToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem cannyToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem sobelToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem laplacianToolStripMenuItem;
+        private System.Windows.Forms.Panel panel1;
+        private Emgu.CV.UI.ImageBox imageBox1;
+        private System.Windows.Forms.ToolStripMenuItem valoresCannyToolStripMenuItem;
+    }
+}
+
---
+++ FormPrincipal.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Emgu;
+using Emgu.CV;
+using Emgu.CV.Structure;
+
+namespace Laplacian_Canny_Sobel_edge_detection
+{
+    public partial class FormPrincipal : Form
+    {
+        //Declaramos una imagen como global
+        Image<Bgr, byte> img;
+
+        #region Inicializar
+        public FormPrincipal()
+        {
+            InitializeComponent();
+        }
+        #endregion
+
+        #region Salir
+        //Para cerrar programa
+        private void salidrToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            //Pedimos confirmación
+            if(MessageBox.Show("¿Está seguro que desea salir?","MENSAJE DE CONFIRMACIÓN",MessageBoxButtons.YesNo)==DialogResult.Yes)
+            {
+                this.Close();
+            }
+        }
+        #endregion
+
+        #region IMPORTAR
+
+        //Para importar la imagen
+        private void importarToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ventanaDialog = new OpenFileDialog();
+            //Si dice que todo se lleva a cabo correctamente
+            if(ventanaDialog.ShowDialog()==DialogResult.OK)
+            {
+                try
+                {
+                    img = new Image<Bgr, byte>(ventanaDialog.FileName);
+                    //Igualamos la imagen subida con la imagen de la ventana
+                    imageBox1.Image = img;
+                    //Podriamos definir el comportamiento del imageBox con 
+                }catch(Exception )
+                {
+                    MessageBox.Show("El archivo seleccionado no es del formato requerido", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+           
+        }
+        #endregion
+
+        private void cannyToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            AplicarCanny();
+
+        }
+
+        public void AplicarCanny(double limite=50.0,double limite2=20.0)
+        {
+            if (img != null)
+            {
+                Image<Gray, byte> imgCanny = new Image<Gray, byte>(img.Width, img.Height, new Gray(0));
+                imgCanny = img.Canny(limite , limite2);
+                imageBox1.Image = imgCanny;
+
+            }
+            else
+            {
+                MessageBox.Show("Debe cargar una imagen para poder encontrar su borde", "Sin imagen", MessageBoxButtons.OK);
+            }
+
+        }
+
+        private void sobelToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (img != null)
+            {
+                //Para el sobel necesitamos una imagen en escala de grises y de extensión float
+
+                Image<Gray, byte> imgGray = img.Convert<Gray,byte>();
+                Image<Gray, float> imgSobel = new Image<Gray, float>(img.Width, img.Height, new Gray(0));
+                imgSobel = imgGray.Sobel(1, 1, 3);
+                imageBox1.Image = imgSobel;
+            }else
+            {
+                MessageBox.Show("Debe cargar una imagen para poder encontrar su borde", "Sin imagen", MessageBoxButtons.OK);
+            }
+}
+
+        private void laplacianToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (img != null)
+            {
+                //Para el sobel necesitamos una imagen en escala de grises y de extensión float
+
+                Image<Gray, byte> imgGray = img.Convert<Gray, byte>();
+                Image<Gray, float> imgLaplace = new Image<Gray, float>(img.Width, img.Height, new Gray(0));
+                imgLaplace = imgGray.Laplace(3);
+                imageBox1.Image = imgLaplace;
+            }
+            else
+            {
+                MessageBox.Show("Debe cargar una imagen para poder encontrar su borde", "Sin imagen", MessageBoxButtons.OK);
+            }
+        }
+
+        private void valoresCannyToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            ValoresCanny cp = new Laplacian_Canny_Sobel_edge_detection.ValoresCanny(this);
+            cp.StartPosition = FormStartPosition.CenterParent;
+            cp.Show();
+        }
+    }
+}
