094a05a4f2a64b0b9fd58364bb075d397bcb25d0
ACO <54153297+ACO-626@users.noreply.github.com>
2020-11-22 01:20:57 -0600

    Add files via upload

---
+++ ValoresCanny.Designer.cs
@@ -0,0 +1,149 @@
+﻿namespace Laplacian_Canny_Sobel_edge_detection
+{
+    partial class ValoresCanny
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.limiteCanny = new System.Windows.Forms.NumericUpDown();
+            this.limiteCanny2 = new System.Windows.Forms.NumericUpDown();
+            this.btnAplicar = new System.Windows.Forms.Button();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.limiteCanny)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.limiteCanny2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.limiteCanny2);
+            this.groupBox1.Controls.Add(this.limiteCanny);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.Location = new System.Drawing.Point(30, 26);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(441, 151);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Canny";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(113, 39);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(49, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Límite:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(37, 95);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(125, 17);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Threshold Linking:";
+            // 
+            // limiteCanny
+            // 
+            this.limiteCanny.Location = new System.Drawing.Point(232, 33);
+            this.limiteCanny.Maximum = new decimal(new int[] {
+            255,
+            0,
+            0,
+            0});
+            this.limiteCanny.Name = "limiteCanny";
+            this.limiteCanny.Size = new System.Drawing.Size(120, 23);
+            this.limiteCanny.TabIndex = 2;
+            // 
+            // limiteCanny2
+            // 
+            this.limiteCanny2.Location = new System.Drawing.Point(232, 95);
+            this.limiteCanny2.Maximum = new decimal(new int[] {
+            255,
+            0,
+            0,
+            0});
+            this.limiteCanny2.Name = "limiteCanny2";
+            this.limiteCanny2.Size = new System.Drawing.Size(120, 23);
+            this.limiteCanny2.TabIndex = 3;
+            // 
+            // btnAplicar
+            // 
+            this.btnAplicar.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.btnAplicar.Location = new System.Drawing.Point(335, 196);
+            this.btnAplicar.Name = "btnAplicar";
+            this.btnAplicar.Size = new System.Drawing.Size(75, 23);
+            this.btnAplicar.TabIndex = 4;
+            this.btnAplicar.Text = "Aplicar";
+            this.btnAplicar.UseVisualStyleBackColor = false;
+            this.btnAplicar.Click += new System.EventHandler(this.btnAplicar_Click);
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.btnCancel.Location = new System.Drawing.Point(426, 196);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(75, 23);
+            this.btnCancel.TabIndex = 5;
+            this.btnCancel.Text = "Cerrar";
+            this.btnCancel.UseVisualStyleBackColor = false;
+            this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
+            // 
+            // ValoresCanny
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(513, 231);
+            this.Controls.Add(this.btnAplicar);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "ValoresCanny";
+            this.Text = "ValoresCanny";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.limiteCanny)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.limiteCanny2)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.NumericUpDown limiteCanny2;
+        private System.Windows.Forms.NumericUpDown limiteCanny;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnAplicar;
+        private System.Windows.Forms.Button btnCancel;
+    }
+}
---
+++ ValoresCanny.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Laplacian_Canny_Sobel_edge_detection
+{
+    public partial class ValoresCanny : Form
+    {
+        FormPrincipal formP;
+        public ValoresCanny()
+        {
+            InitializeComponent();
+        }
+
+        public  ValoresCanny(FormPrincipal fm)
+        {
+            InitializeComponent();
+            formP = fm;
+        }
+
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btnAplicar_Click(object sender, EventArgs e)
+        {
+            if(formP!=null)
+            {
+                formP.AplicarCanny((double)limiteCanny.Value,(double)limiteCanny2.Value);
+            }
+        }
+    }
+}
